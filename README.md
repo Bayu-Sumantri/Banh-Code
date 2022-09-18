bbbf8ed40594e2589cf434eca3250350c861a07c
Bayu Sumantri <bayusuman09@gmail.com>
2022-09-18 17:10:34 +0700

    Banh Code

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 <p>This website is still under development and we are still trying to work</p>
 <p> on the buds in our plans and we will fix it soon and this is just an update with bugs</p>
+<p>final result</p>
--- transaction.php
+++ transaction.php
@@ -18,22 +18,22 @@
           <div class="col-50">
             <h3>Billing Address</h3>
             <label for="fname"><i class="fa fa-user"></i> Full Name</label>
-            <input type="text" id="fname" name="firstname" placeholder="John M. Doe">
+            <input type="text" id="fname" name="firstname" placeholder="icikiwir">
             <label for="email"><i class="fa fa-envelope"></i> Email</label>
-            <input type="text" id="email" name="email" placeholder="john@example.com">
+            <input type="text" id="email" name="email" placeholder="icikiwir">
             <label for="adr"><i class="fa fa-address-card-o"></i> Alamat</label>
-            <input type="text" id="adr" name="address" placeholder="542 W. 15th Street">
+            <input type="text" id="adr" name="address" placeholder="icikiwir">
             <label for="city"><i class="fa fa-institution"></i> Kota</label>
-            <input type="text" id="city" name="city" placeholder="New York">
+            <input type="text" id="city" name="city" placeholder="icikiwir">
 
             <div class="row">
               <div class="col-50">
                 <label for="state">Negara</label>
-                <input type="text" id="state" name="state" placeholder="NY">
+                <input type="text" id="state" name="state" placeholder="icikiwir">
               </div>
               <div class="col-50">
                 <label for="zip">Zip</label>
-                <input type="text" id="zip" name="zip" placeholder="10001">
+                <input type="text" id="zip" name="zip" placeholder="icikiwir">
               </div>
             </div>
           </div>
@@ -48,19 +48,19 @@
               <i class="fa fa-cc-discover" style="color:orange;"></i>
             </div>
             <label for="cname">Nama di kartu</label>
-            <input type="text" id="cname" name="cardname" placeholder="John More Doe">
+            <input type="text" id="cname" name="cardname" placeholder="icikiwir">
             <label for="ccnum">Nomor kartu kredit</label>
-            <input type="text" id="ccnum" name="cardnumber" placeholder="1111-2222-3333-4444">
+            <input type="text" id="ccnum" name="cardnumber" placeholder="icikiwir">
             <label for="expmonth">Bulan Kedaluwarsa</label>
-            <input type="text" id="expmonth" name="expmonth" placeholder="September">
+            <input type="text" id="expmonth" name="expmonth" placeholder="icikiwir">
             <div class="row">
               <div class="col-50">
                 <label for="expyear">Tahun Kedaluwarsa</label>
-                <input type="text" id="expyear" name="expyear" placeholder="2018">
+                <input type="text" id="expyear" name="expyear" placeholder="icikiwir">
               </div>
               <div class="col-50">
                 <label for="cvv">CVV</label>
-                <input type="text" id="cvv" name="cvv" placeholder="352">
+                <input type="text" id="cvv" name="cvv" placeholder="icikiwir">
               </div>
             </div>
           </div>
